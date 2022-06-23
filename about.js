cf633bc879824b9ddc1d38ae7f2d628e4fbfac9e
carnage999-max <105145697+carnage999-max@users.noreply.github.com>
2022-06-23 17:12:05 +0100

    Add files via upload

---
+++ about.js
@@ -0,0 +1,5 @@
+my_name = "Ezekiel Okebule";
+my_country = "Nigeria";
+my_height_cm = 180.34;
+my_height_ft = (my_height_cm / 30.48).toFixed(2);
+console.log(`My name is ${my_name}, I am ${my_height_cm}cm or ${my_height_ft}ft tall. I was born and I live in ${my_country}`);
